ea2635a279a420284d03178c10ec6a4a27306d25
Rafael-SzBaptista <rafael.baptista@alltis.com.br>
2026-03-25 12:33:01 -0300

    Fix: Fix Scroll Top Automatic

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider } from "@/contexts/AuthContext";
+import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import CriarJogo from "./pages/CriarJogo";
 import Presentes from "./pages/Presentes";
@@ -27,6 +28,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <AuthProvider>
           <Routes>
             <Route path="/" element={<Index />} />
